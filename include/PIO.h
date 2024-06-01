65520134df33fc65805f0dd74ec302d32682a5cc
lirubao <lirubaogo@gmail.com>
2024-06-02 03:04:23 +0800

    LED 流水灯

---
+++ include/PIO.h
@@ -0,0 +1,10 @@
+// A0-A7引脚
+#define PA0 (GPIO_Pin_0)     // PIO A0引脚
+#define PA1 (GPIO_Pin_1)     // PIO A0引脚
+#define PA2 (GPIO_Pin_2)     // PIO A0引脚
+#define PA3 (GPIO_Pin_3)     // PIO A0引脚
+#define PA4 (GPIO_Pin_4)     // PIO A0引脚
+#define PA5 (GPIO_Pin_5)     // PIO A0引脚
+#define PA6 (GPIO_Pin_6)     // PIO A0引脚
+#define PA7 (GPIO_Pin_7)     // PIO A0引脚
+#define PAAll (GPIO_Pin_All) // PIO A0引脚
--- src/main.c
+++ src/main.c
@@ -1,20 +1,22 @@
 #include <stm32f10x.h>
 #include <Delay.h>
+#include <PIO.h>
 
 // Alternates blue and green LEDs quickly
 int main(void) {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Pin = PAAll;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
-
+    // A0-A7引脚
+    uint16_t pio_as[8] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
     while (1) {
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
-        Delay_ms(100);
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
-        Delay_ms(100);
+        for (int i = 0; i < 8; i++) { /* code */
+            GPIO_Write(GPIOA, ~pio_as[i]);
+            Delay_ms(100);
+        }
     }
     return 0;
 }
