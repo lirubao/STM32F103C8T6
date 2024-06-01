260f34b49b1b3660bb502b97c7411cb21e7e24cd
lirubao <lirubaogo@gmail.com>
2024-06-02 02:20:16 +0800

    LED PA0闪烁

--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
 #include <stm32f10x.h>
+#include <Delay.h>
 
 // Alternates blue and green LEDs quickly
 int main(void) {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-    GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    while (1) {}
+    while (1) {
+        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+        Delay_ms(100);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+        Delay_ms(100);
+    }
     return 0;
 }
